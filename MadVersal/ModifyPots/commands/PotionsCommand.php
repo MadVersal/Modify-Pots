d8ce38865518a92db232cf10fd935ae954291d92
MadVersal <zuriivalentin@gmail.com>
2025-05-13 08:24:13 -0600

    Create PotionsCommand.php

---
+++ MadVersal/ModifyPots/commands/PotionsCommand.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace MadVersal\ModifyPots\command;
+
+use MadVersal\ModifyPots\Loader;
+use pocketmine\utils\TextFormat;
+use pocketmine\player\Player;
+use pocketmine\command\CommandSender;
+
+class PotionsCommand extends Command
+{
+  public function __construct ()
+{
+    parent::construct('potions', 'use command to edit potions health');
+    $this->setPermission('potions.command);
+}   
+               
