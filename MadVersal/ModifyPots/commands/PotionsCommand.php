9ef69bfd1b8f8a74f5662d0cbb966a0a5180797a
MadVersal <zuriivalentin@gmail.com>
2025-05-13 08:36:21 -0600

    Update PotionsCommand.php

--- MadVersal/ModifyPots/commands/PotionsCommand.php
+++ MadVersal/ModifyPots/commands/PotionsCommand.php
@@ -9,7 +9,7 @@ use pocketmine\command\CommandSender;
 
 class PotionsCommand extends Command
 {
-  public function __construct ()
+  public function __construct()
 {
     parent::construct('potions', 'use command to edit potions health');
     $this->setPermission('potions.command);
